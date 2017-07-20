1f8dab06ecdd73e66e22dfef3827d7c9f23d7e20
xboy2012 <xboy2008@live.cn>
2017-07-20 19:36:29 +0800

    remove htmlmin

--- src/js/index.js
+++ src/js/index.js
@@ -3,25 +3,20 @@ import css_code from './_generated/css';
 import cheerio from 'cheerio';
 import juice from 'juice/client';
 import csso from 'csso';
-import htmlmin from 'htmlmin';
 import formatNode from './formatNode';
 import calculateImageSize from './calculateImageSize';
 
 import Types from './types';
 import config from './_generated/config';
 
-const build = (node, opts) => {
-    opts = Object.assign({
-        minify: true
-    }, opts);
-
+const build = (node) => {
     node = formatNode(node);
     return calculateImageSize(node).then(() => {
         let html_code = templateFn({node});
 
         let $ = cheerio.load(html_code, {
-            lowerCaseTags: opts.minify,
-            lowerCaseAttributeNames: opts.minify,
+            lowerCaseTags: true,
+            lowerCaseAttributeNames: true,
             decodeEntities: false,
             recognizeSelfClosing: true
         });
@@ -30,33 +25,20 @@ const build = (node, opts) => {
             applyAttributesTableElements: true
         });
 
+        //删除id和class
+        $('[id]').removeAttr('id');
+        $('[class]').removeAttr('class');
 
-        if(opts.minify) {
-            //删除id和class
-            $('[id]').removeAttr('id');
-            $('[class]').removeAttr('class');
-
-            //压缩优化style
-            $('[style]').each((i, el) => {
-                let $el = $(el);
-                let style = $el.attr('style');
-                style = csso.minifyBlock(style, {}).css;
-                $el.attr('style', style);
-            });
-        }
+        //压缩优化style
+        $('[style]').each((i, el) => {
+            let $el = $(el);
+            let style = $el.attr('style');
+            style = csso.minifyBlock(style, {}).css;
+            $el.attr('style', style);
+        });
 
 
         html_code = $.html();
-
-        if(opts.minify) {
-            html_code = htmlmin(html_code, {
-                cssmin: false,
-                jsmin: false,
-                removeComments: true,
-                collapseWhitespace: true
-            });
-        }
-
         return html_code;
     });
 };
